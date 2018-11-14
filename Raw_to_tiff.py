efe8236765d8be809a7b04e08bcf52827bd96db1
Kharitonov <kharitok@win.desy.de>
2018-11-14 11:51:04 +0100

    Ready to use

--- Raw_to_tiff.py
+++ Raw_to_tiff.py
@@ -1,58 +1,39 @@
 import argparse
-import h5py
-from tifffile import imwrite, imread
-import re
 import os
+import re
 import sys
+
+
 import numpy as np
-
-import matplotlib.pyplot as plt
-
-import rawpy
-import imageio
-import PIL
-
-f = 'FL24_EXP2_20181029T183004_371_260729937.raw'
+from tifffile import imread, imwrite
 
 
-def raw_to_array(filename, res=16):
+def raw_to_array(filename, downscale=False):
+    """Convert file from 16bit little endian .raw to .tiff
+    filename - name of file t be converted
+    downscale - if color depth shoud be downscaled to 8 bit"""
 
     with open(filename, 'rb') as file_:
         file_.seek(15, os.SEEK_SET)
-        arr = np.fromfile(file_, dtype=np.dtype('<i2'))
+        arr = np.fromfile(file_, dtype=np.dtype('<u2'))
         arr = arr.reshape([960, 1278])
         arr = np.fliplr(arr)
         arr = np.flipud(arr)
-        imwrite(filename[0:-3]+'tif', arr, arr.shape, dtype=np.dtype('i2'))
+        if downscale:
+            arr = arr / (arr.max() / (2**8-1))
+            # arr[arr>=2**8] = 2**8-1
+            arr = arr.astype(dtype=np.dtype('u1'))
+            imwrite(filename[0:-4]+'_8b_.tif', arr, arr.shape, dtype=np.dtype('u1'))
+            return
+        else:
+            imwrite(filename[0:-4]+'_16b_.tif', arr, arr.shape, dtype=np.dtype('u2'))
+
         # plt.imshow(arr)
         # plt.show()
 
 
-"""
-file_=open(f, 'rb')
-file_.seek(15, os.SEEK_SET)
-arr = np.fromfile(file_, dtype=np.dtype('<i2'))
-arr.shape
-arr = arr.reshape([960, 1278])
-arr = np.fliplr(arr)
-arr = np.flipud(arr)
-
-imwrite('templ.tif', err,err.shape,dtype=np.dtype('i1'))
-imwrite('temp.tif', arr,arr.shape,dtype=np.dtype('i2'))
-brr = imread('temp.tif')
-initial = imread('FL24_EXP2_20181029T183004_371_260729937.tif')
-corrected = imread('FL24_EXP2_20181029T183004_371_260729937LE.tif')
-plt.imshow(arr)
-plt.show()
-plt.imshow(brr)
-plt.show()
-plt.imshow(initial)
-plt.show()
-plt.imshow(corrected)
-plt.show()"""
-
-
 def Create_filelist(full_path):
+    """Create list of .raw files in directory or get single filename"""
 
     if not os.path.isdir(full_path):
 
@@ -65,10 +46,12 @@ def Create_filelist(full_path):
     return all_files
 
 
-def conv(path):
+def conv(path, downscale=False):
+    """ convert all files in path directory or
+    only file with path = path using raw_to_array() dunction"""
     count = 0
     for i in Create_filelist(path):
-        raw_to_array(i)
+        raw_to_array(i, downscale=downscale)
         count += 1
 
     print(f'converted {count} files')
@@ -81,7 +64,12 @@ if __name__ == "__main__":
                                      formatter_class=argparse.RawTextHelpFormatter,
                                      epilog=""" """)
 
-    parser.add_argument('Path', metavar='Path', type=str,
+    parser.add_argument('Path', metavar='P', type=str,
                         help='Path to folder or file to  be converted')
+    parser.add_argument('D', metavar='D', type=bool, nargs='?', const=False,
+                        help="""If image need to be downscaled to 8 bit using mapping from [min_val -  max_val] to [0 - 255]""")
     args = parser.parse_args()
-    conv(args.Path)
+    if args.D is None:
+        conv(args.Path)
+    else:
+        conv(args.Path, downscale=True)
